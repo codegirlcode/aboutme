4ade4287c08a27bdf437f04b0aca8acff7e2a3a9
h <codegirlcodexo@gmail.com>
2016-03-27 02:07:36 -0400

    cleaned css, and deteled img files

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-main.js
+<script type='text/javascript'>
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
+</script>
